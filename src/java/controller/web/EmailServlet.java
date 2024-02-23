ebd90c3313c82a253a3a35314c723e501562e03b
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-02-23 14:54:33 +0700

    send email in contact.jsp - failded

---
+++ src/java/controller/web/EmailServlet.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Email;
+
+/**
+ *
+ * @author lvhho
+ */
+@WebServlet(name = "EmailServlet", urlPatterns = {"/EmailServlet"})
+public class EmailServlet extends HttpServlet {
+
+    private static final String CONTACT_PAGE = "contact.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = CONTACT_PAGE;
+        try {
+            Email handleEmail = new Email();
+            String message = "Có vẻ như một số thông tin cung cấp của bạn không hợp lệ, vui lòng cung cấp lại thông tin";
+            String check = "fail";
+            String action = request.getParameter("action");
+            String name = request.getParameter("name");
+            String email = request.getParameter("email");
+            String txt = request.getParameter("message");
+            if (action != null) {
+
+                if (handleEmail.isValidEmail(email)) {
+                    message = "Cảm ơn bạn là liên hệ với chúng tôi, chúng tôi sẽ kết nối với bạn trong thời gian sớm nhất";
+                    check = "success";
+                    String sub = handleEmail.subjectContact(name);
+                    String msg = handleEmail.messageContact(name);
+                    handleEmail.sendEmail(sub, msg, email);
+                }
+                request.setAttribute("MESSAGE", message);
+                request.setAttribute("CHECK", check);
+                request.setAttribute("NAME_CUSTOMER", name);
+                request.setAttribute("EMAIL_CUSTOMER", email);
+                request.setAttribute("TEXT", txt);
+            } else {
+                request.setAttribute("MESSAGE", message);
+                request.setAttribute("CHECK", check);
+                request.setAttribute("NAME_CUSTOMER", name);
+                request.setAttribute("EMAIL_CUSTOMER", email);
+                request.setAttribute("TEXT", txt);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
