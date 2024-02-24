22f9af55ef21937be30e9226314bd94da256f78f
phuuthanh2003 <phuuthanh2003@gmail.com>
2024-02-24 13:27:16 +0700

    Store: add type to product

---
+++ src/java/model/TypeDTO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author HuuThanh
+ */
+public class TypeDTO {
+     private int typeId;
+    private String name;
+
+    public TypeDTO() {
+    }
+
+    public TypeDTO(int typeId, String name) {
+        this.typeId = typeId;
+        this.name = name;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
