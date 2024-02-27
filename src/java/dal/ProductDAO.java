06cad3c193cffb3cb0fae04e137de2f9d8d6bf51
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-02-27 22:14:58 +0700

    Store: single-product.jsp (50%)

--- src/java/dal/ProductDAO.java
+++ src/java/dal/ProductDAO.java
@@ -13,7 +13,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import model.CategoryDTO;
 import model.ProductDTO;
